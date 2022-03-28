ba076c50bcc92c0869688970841eb17de62c5300
test <test@10.0.0.10>
2022-03-27 17:51:51 -0700

    Adding new java project

---
+++ com.javaproject.test/src/test/java/com/javaproject/test/AddNumbersTest.java
@@ -0,0 +1,15 @@
+package com.javaproject.test;
+
+import org.testng.annotations.Test;
+
+public class AddNumbersTest {
+	
+	@Test
+	public void AddNumberTest1() {
+		int a =9;
+		int b=3;
+		int sum= a+b;
+		System.out.println(sum);
+	}
+
+}
